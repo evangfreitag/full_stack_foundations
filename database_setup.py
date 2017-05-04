20e0adb6351e0fffcaf1f2cd9989b48a32bdd5d1
Evan Freitag <evangfreitag@gmail.com>
2017-05-03 22:33:11 -0400

    First Commit

---
+++ database_setup.py
@@ -0,0 +1,24 @@
+import sys
+from sqlalchemy importColumn, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import deeclarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary key = True)
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+engine = create_engine('sqlite:///restaurantmenu.db)
+Base.metadata.create_all(engine)
